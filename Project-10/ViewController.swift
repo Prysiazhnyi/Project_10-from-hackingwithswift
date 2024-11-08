200c5a360ddcbd34c3525083ae04d2eed74e48cb
sprysiazhnyi <sprysiazhnyi@competo.io>
2024-11-08 10:24:20 +0200

    Initial Commit

---
+++ Project-10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project-10
+//
+//  Created by Serhii Prysiazhnyi on 08.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
